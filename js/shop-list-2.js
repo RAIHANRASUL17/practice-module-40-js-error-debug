ca669055b43115b5a400ef9ce1348b4bf4804a2a
RAIHANRASUL17 <smrihanrasul17cqut@gmail.com>
2023-03-09 11:09:56 +0800

    set golobal addinput fiel and add button

--- js/shop-list-2.js
+++ js/shop-list-2.js
@@ -1,12 +1,10 @@
-/*__________ set golobal variable start ______________*/ 
-/*_---------- add button part start --------*/    
-// get inputField
+
+function addItem(){
+    // console.log('set onclick add button is working or not , check')
+    // get inputField
     const addInputField= document.getElementById('add-field')
     const addInput=addInputField.value;
     addInputField.value= ''
-    /*_---------- add button part end --------*/   
-/*__________ set golobal variable end   ______________*/ 
-function addItem(){
     // step-1
     const itemList = document.getElementById('item-list')
     //step-2 create li 
